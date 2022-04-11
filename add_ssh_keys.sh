aa3f3f7eda862686a4dfc1d398986e358e98812b
Daryl <DarylCantrell@users.noreply.github.com>
2022-04-11 09:22:38 -0400

    Update add_ssh_keys.sh

--- add_ssh_keys.sh
+++ add_ssh_keys.sh
@@ -1,10 +1,20 @@
-#! /bin/sh
+#! /bin/bash
 
-monaPubKey=`sed 's/= .*/=/' ~/dotfiles/testKeys/monalisa.rsa.pub`
-monaFingerprint=$(ssh-keygen -l -E SHA256 -f /root/dotfiles/testKeys/monalisa.rsa.pub | /bin/sed -e 's/.*SHA256://g' -e 's/ .*//g')
+# Parse dev SSH key files for monalisa and collaborator:
 
-collabPubKey=`sed 's/= .*/=/' ~/dotfiles/testKeys/collaborator.rsa.pub`
-collabFingerprint=$(ssh-keygen -l -E SHA256 -f /root/dotfiles/testKeys/collaborator.rsa.pub | /bin/sed -e 's/.*SHA256://g' -e 's/ .*//g')
+monaPubKey=`sed 's/= .*/=/' ~/dotfiles/devKeys/monalisa.rsa.pub`
+monaFingerprint=$(
+	ssh-keygen -l -E SHA256 -f /root/dotfiles/devKeys/monalisa.rsa.pub |
+	/bin/sed -e 's/[^:]*SHA256:\([^ ]*\) .*/\1/g'
+)
+
+collabPubKey=`sed 's/= .*/=/' ~/dotfiles/devKeys/collaborator.rsa.pub`
+collabFingerprint=$(
+	ssh-keygen -l -E SHA256 -f /root/dotfiles/devKeys/collaborator.rsa.pub |
+	/bin/sed -e 's/[^:]*SHA256:\([^ ]*\) .*/\1/g'
+)
+
+# Insert dev keys into public_keys table:
 
 mysql -D github_development << EOF
 SELECT id INTO @mona_id FROM users WHERE login = 'monalisa';
