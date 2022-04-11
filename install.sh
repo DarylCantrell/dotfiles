14d1f18d613f45976d555961cd18894b7459c834
Daryl <DarylCantrell@users.noreply.github.com>
2022-04-11 21:14:20 +0000

    Chmod and other changes

--- install.sh
+++ install.sh
@@ -18,16 +18,14 @@ test -f /workspaces/.codespaces/.persistedshare/dotfiles/.dircolors &&
     ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.dircolors ~/.dircolors
 
 # Stuff to do only on github/github
-if [ -f /workspaces/github/README.md -a -d /workspaces/.codespaces/.persistedshare/dotfiles/testKeys ]; then
-    # add and trust monalisa GPG key
-    test -f /workspaces/github/README.md -a -r /workspaces/.codespaces/.persistedshare/dotfiles/testKeys/monalisa.gpg.sec &&
-        gpg --import /workspaces/.codespaces/.persistedshare/dotfiles/testKeys/monalisa.gpg.sec &&
+if [ -f /workspaces/github/README.md -a -d /workspaces/.codespaces/.persistedshare/dotfiles/devKeys ]; then
+
+    # Add and trust monalisa GPG key
+    test -f /workspaces/github/README.md -a -r /workspaces/.codespaces/.persistedshare/dotfiles/devKeys/monalisa.gpg.sec &&
+        gpg --import /workspaces/.codespaces/.persistedshare/dotfiles/devKeys/monalisa.gpg.sec &&
         echo 27A08E3AFB8CDD4C0D4FE226AD3B4A12FAD9D319:6: | gpg --import-ownertrust
 
-    # SSH private key files will be ignored with git's default 644 permissions
-    chmod 600 /workspaces/.codespaces/.persistedshare/dotfiles/testKeys/*.rsa
-
-    # clon repo -- probably put this in its own script
-    # GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/testKeys/monalisa.rsa)" git clone ssh://git@localhost:3035/github/public-server.git
-    # git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/testKeys/monalisa.rsa)"
+    # Some private key files will be ignored if they have git's default 644 permissions
+    chmod 600 /workspaces/.codespaces/.persistedshare/dotfiles/devKeys/*.rsa \
+        /workspaces/.codespaces/.persistedshare/dotfiles/devKeys/*.gpg.sec
 fi
