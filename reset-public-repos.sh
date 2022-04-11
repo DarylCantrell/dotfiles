71050d994a95de5eb8f0e90ffc84bc7a2affbdae
Daryl <DarylCantrell@users.noreply.github.com>
2022-04-11 09:30:04 -0400

    Update reset-public-repos.sh

--- reset-public-repos.sh
+++ reset-public-repos.sh
@@ -2,20 +2,26 @@
 
 pushd /workspaces
 
-# monalisa clone public-server
+# Clone public-server as monalisa
+
 rm -rf /workspaces/public-server
 mkdir /workspaces/public-server
 cd /workspaces/public-server
 
-GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/testKeys/monalisa.rsa)" git clone ssh://git@localhost:3035/github/public-server.git .
-git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/testKeys/monalisa.rsa)"
+GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/monalisa.rsa)" \
+  git clone ssh://git@localhost:3035/github/public-server.git .
 
-# collaborator clone public-server
+git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/monalisa.rsa)"
+
+# Clone public-server as collaborator
+
 rm -rf /workspaces/collab-public-server
 mkdir /workspaces/collab-public-server
 cd /workspaces/collab-public-server
 
-GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/testKeys/collaborator.rsa)" git clone ssh://git@localhost:3035/github/public-server.git .
-git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/testKeys/collaborator.rsa)"
+GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/collaborator.rsa)" \
+  git clone ssh://git@localhost:3035/github/public-server.git .
+  
+git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/collaborator.rsa)"
 
 popd
