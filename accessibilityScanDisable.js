5a8c27516b3b05d0120be2c292856c37a227bccc
Daryl <DarylCantrell@users.noreply.github.com>
2022-08-18 16:45:53 +0000

    Add script to set accessibility check disable cookie

---
+++ accessibilityScanDisable.js
@@ -0,0 +1,2 @@
+document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax"
+document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax; secure"
