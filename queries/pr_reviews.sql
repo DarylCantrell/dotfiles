9608838733e1fbb1dbe8c9b7120115fbfd8071f0
Daryl <DarylCantrell@users.noreply.github.com>
2023-01-26 08:13:01 -0800

    Update queries/pr_reviews.sql

--- queries/pr_reviews.sql
+++ queries/pr_reviews.sql
@@ -1,9 +1,21 @@
 select
-  concat(pr.id, "  ", base_ref, " → ", head_ref) as pr_id,
+  concat(pr.id, "  (", pr.head_ref, " → ", pr.base_ref, ")") as pr_id,
   left(pr.base_sha, 8) as pr_base_sha,
   left(pr.head_sha, 8) as pr_head_sha,
-  left(pr.merge_commit_sha, 8) as pr_merge_commit_sha,
-  pr.base_sha_on_merge as pr_base_sha_on_merge
+  pr.mergeable as pr_mergeable,
+  -- left(pr.merge_commit_sha, 8) as pr_merge_commit_sha,
+  pr.merge_commit_sha,
+  pr.base_sha_on_merge as pr_base_sha_on_merge,
+  rvw.id as rvw_id,
+  rvw.user_id as rvw_user_id,
+  left(rvw.head_sha, 8) as rvw_head_sha,
+  left(rvw.merge_base_sha, 8) as rvw_merge_base_sha,
+  case rvw.state
+    when 0 then '0:pending'     when 1 then '1:commented'    when 30 then '30:changes_requested'
+    when 40 then '40:approved'  when 50 then '50:dismissed'  else rvw.state
+  end as rvw_state
 from github_development_issues_pull_requests.pull_requests pr
+left outer join github_development_issues_pull_requests.pull_request_reviews rvw
+  on pr.id = rvw.pull_request_id
 where
   true\G
