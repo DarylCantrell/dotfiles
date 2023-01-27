e2e6997709dc02911fb50a7100463eb14ba99445
Daryl <DarylCantrell@users.noreply.github.com>
2023-01-27 07:52:34 -0800

    Update accessibilityScanDisable.js

--- accessibilityScanDisable.js
+++ accessibilityScanDisable.js
@@ -1,2 +1,4 @@
 document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax"
 document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax; secure"
+document.cookie="accessibilityScan=false; path=/; domain=github.localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax"
+document.cookie="accessibilityScan=false; path=/; domain=github.localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax; secure"
