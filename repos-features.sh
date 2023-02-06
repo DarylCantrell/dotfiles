81d977da51a5962c96cccada87a3b06d03a0ad33
Daryl <DarylCantrell@users.noreply.github.com>
2023-02-06 15:02:50 -0800

    Chmod script +x

--- repos-features.sh
+++ repos-features.sh
@@ -1,3 +1,5 @@
 bin/toggle-feature-flag enable edit_repo_protections_fine_grained_perm
 bin/toggle-feature-flag enable push_policies
 bin/toggle-feature-flag enable repo_org_rules
+bin/toggle-feature-flag enable pull_request_review_merge_base_sha
+bin/toggle-feature-flag enable pull_request_strict_review_policy
