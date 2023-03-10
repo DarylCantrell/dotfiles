3fe7445e20fc0233b4f4f532f55820b3954967b0
Daryl <DarylCantrell@users.noreply.github.com>
2023-03-10 07:42:09 -0800

    Add last pusher to repos FF list

--- repos-features.sh
+++ repos-features.sh
@@ -1,5 +1,6 @@
 bin/toggle-feature-flag enable edit_repo_protections_fine_grained_perm
 bin/toggle-feature-flag enable push_policies
 bin/toggle-feature-flag enable repo_org_rules
+bin/toggle-feature-flag enable last_pusher_policy
 bin/toggle-feature-flag enable pull_request_review_merge_base_sha
 bin/toggle-feature-flag enable pull_request_strict_review_policy
