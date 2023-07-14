d266f1c92d90a54be3567eeb4308b6c35f3fbd4f
Daryl <DarylCantrell@users.noreply.github.com>
2023-07-14 11:41:41 -0700

    New curl commands

--- curl-cookbook.sh
+++ curl-cookbook.sh
@@ -1,14 +1,19 @@
-create_pull_request() {
+usage_error() {
+  echo $1
+  echo
+  echo "<base_url> samples:"
+  echo '  http://api.github.localhost/repos/{owner}/{repo}'
+  echo '  https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
+}
+
+pr_create() {
   base_url=$1; shift
   auth_token=$1; shift
   source_branch=$1; shift
   target_branch=$1; shift
 
   if [ -z "$target_branch" ]; then
-    echo "Usage: $0 <base_url> <auth_token> <source_branch> <target_branch>"
-    echo "<base_url> samples:"
-    echo '  http://api.github.localhost/repos/{owner}/{repo}'
-    echo '  https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
+    usage_error "Usage: pr_create <base_url> <auth_token> <source_branch> <target_branch>"
     return 1
   fi
 
@@ -26,16 +31,13 @@ create_pull_request() {
     $base_url"/pulls"
 }
 
-close_pull_request() {
+pr_close() {
   base_url=$1; shift
   auth_token=$1; shift
   pr_number=$1; shift
 
   if [ -z "$pr_number" ]; then
-    echo "Usage: $0 <base_url> <auth_token> <pr_number>"
-    echo "<base_url> samples:"
-    echo '  http://api.github.localhost/repos/{owner}/{repo}'
-    echo '  https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
+    usage_error "Usage: pr_close <base_url> <auth_token> <pr_number>"
     return 1
   fi
 
@@ -48,16 +50,13 @@ close_pull_request() {
     $base_url"/pulls/"$pr_number
 }
 
-approve_pull_request() {
+pr_approve() {
   base_url=$1; shift
   auth_token=$1; shift
   pr_number=$1; shift
 
   if [ -z "$pr_number" ]; then
-    echo "Usage: $0 <base_url> <auth_token> <pr_number>"
-    echo "<base_url> samples:"
-    echo '  http://api.github.localhost/repos/{owner}/{repo}'
-    echo '  https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
+    usage_error "Usage: pr_approve <base_url> <auth_token> <pr_number>"
     return 1
   fi
 
@@ -68,6 +67,49 @@ approve_pull_request() {
     --header "Authorization: token $auth_token" \
     --data '{ "event":"APPROVE" }' \
     $base_url"/pulls/"$pr_number"/reviews"
+}
+
+pr_merge() {
+  base_url=$1; shift
+  auth_token=$1; shift
+  pr_number=$1; shift
+  sha=$1; shift
+  merge_method=$1; shift
+
+  if [ -z "$sha" ]; then
+    usage_error "Usage: pr_merge <base_url> <auth_token> <pr_number> <sha> [<merge_method>]"
+    return 1
+  fi
+
+  echo \
+    --request PUT -i \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    --data '{
+      "sha": "'$sha'",
+      "merge_method": "'${merge_method:-"merge"}'"
+    }' \
+    $base_url"/pulls/"$pr_number"/merge"
+}
+
+pr_close() {
+  base_url=$1; shift
+  auth_token=$1; shift
+  pr_number=$1; shift
+
+  if [ -z "$pr_number" ]; then
+    usage_error "Usage: pr_close <base_url> <auth_token> <pr_number>"
+    return 1
+  fi
+
+  curl \
+    --request PATCH -i \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    --data '{ "state":"closed" }' \
+    $base_url"/pulls/"$pr_number
 }
 
 exit
