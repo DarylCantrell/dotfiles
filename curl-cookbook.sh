d746e64d81f02bb0b5a297ecff9e0480c0f94490
Daryl <DarylCantrell@users.noreply.github.com>
2023-07-26 09:33:46 -0700

    Updating curl file

--- curl-cookbook.sh
+++ curl-cookbook.sh
@@ -1,9 +1,9 @@
 usage_error() {
   echo $1
   echo
-  echo "<base_url> samples:"
-  echo '  http://api.github.localhost/repos/{owner}/{repo}'
-  echo '  https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
+  echo "<base_url> examples:"
+  echo '  CODESPACE:    http://api.github.localhost/repos/{owner}/{repo}'
+  echo '  GHE TEST ENV: https://api.darylcantrell-03d56231eb10a1a11.ghe-test.org/repos/{owner}/{repo}'
 }
 
 pr_create() {
@@ -110,6 +110,62 @@ pr_close() {
     --header "Authorization: token $auth_token" \
     --data '{ "state":"closed" }' \
     $base_url"/pulls/"$pr_number
+}
+
+branch_get() {
+  base_url=$1; shift
+  auth_token=$1; shift
+  branch_name=${1#"refs/heads/"}; shift
+
+  if [ -z "$branch_name" ]; then
+    usage_error "Usage: branch_get <base_url> <auth_token> <branch_name>"
+    return 1
+  fi
+
+  curl \
+    --request GET -i \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    $base_url"/branches/"$branch_name
+}
+
+ref_get() {
+  base_url=$1; shift
+  auth_token=$1; shift
+  branch_name=${1#"refs/heads/"}; shift
+
+  if [ -z "$branch_name" ]; then
+    usage_error "Usage: ref_get <base_url> <auth_token> <branch_name>"
+    return 1
+  fi
+
+  curl \
+    --request GET -i \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    $base_url"/git/refs/heads/"$branch_name
+}
+
+ref_force() {
+  base_url=$1; shift
+  auth_token=$1; shift
+  branch_name=${1#"refs/heads/"}; shift
+  sha=$1; shift
+
+  if [ -z "$sha" ]; then
+    usage_error "Usage: brancref_forceh_force <base_url> <auth_token> <branch_name> <commit_sha>"
+    return 1
+  fi
+
+  curl \
+    --request PATCH -i \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    --data '{ "sha": "'$sha'", "force": true }' \
+    $base_url"/git/refs/heads/"$branch_name
 }
 
 exit
