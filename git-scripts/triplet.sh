055c86c19349759afa34544e28536fa3c93e7859
Daryl <DarylCantrell@users.noreply.github.com>
2023-09-13 14:11:51 -0700

    minor changes

--- git-scripts/triplet.sh
+++ git-scripts/triplet.sh
@@ -18,6 +18,30 @@ single() {
   pr_approve $url `cat /workspaces/collab.pat` $pr_id
 }
 
+double() {
+  n=$1; shift;
+    if [ -z "$n" ]; then
+    return 1
+  fi
+  local url=http://api.github.localhost/repos/github/public-server
+  local mona_pat=`cat /workspaces/mona.pat`
+
+  git checkout main
+  git pull
+
+  pushnewbranch topic$n"a"
+  force topic$n"b" topic$n"a"
+  git push origin topic$n"b"
+
+  git checkout main
+
+  pr_create $url $mona_pat topic$n"a" main
+  local pr_b=`pr_create $url $mona_pat topic$n"b" main`
+
+  pr_b_id=`sed '/\r$/d' <<< "$pr_b" | jq -r .id`
+  pr_approve $url `cat /workspaces/collab.pat` $pr_b_id
+}
+
 triplet() {
   n=$1; shift;
     if [ -z "$n" ]; then
