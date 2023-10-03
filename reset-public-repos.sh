e09b5545650631700a93b5be5846dc5b609999c5
Daryl <DarylCantrell@users.noreply.github.com>
2023-10-03 10:23:35 -0700

    Clean up reset-public-repos script

--- reset-public-repos.sh
+++ reset-public-repos.sh
@@ -1,62 +1,38 @@
 #! /bin/bash
 
 # This blows away your local changes (if any), and makes new clones of public-server for monalisa, collaborator, and outsider.
-# You will want to run `add_ssh_keys.sh`` first.
+# You will want to run `add_ssh_keys.sh` first.
+
+clone_repo() {
+  local login=$1; shift
+	local name=$1; shift
+	local email=$1; shift
+
+	rm -rf /workspaces/${name}-public-server
+	mkdir /workspaces/${name}-public-server
+	cd /workspaces/${name}-public-server
+
+	GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)" \
+		git clone ssh://git@localhost:3035/github/public-server.git .
+
+	git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)"
+
+	git config --local --unset user.name
+	git config --local --add user.name ${login}
+
+	git config --local --unset user.email
+	git config --local --add user.email ${email}@github.com
+
+	git config --local gpg.format ssh
+	git config --local user.signingkey "$(realpath ~/dotfiles/devKeys/${login}.ed25519)"
+	git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
+	git config --local commit.gpgsign true
+}
 
 pushd /workspaces
 
-# Clone public-server as monalisa
-
-rm -rf /workspaces/mona-public-server
-mkdir /workspaces/mona-public-server
-cd /workspaces/mona-public-server
-
-GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/monalisa.ed25519)" \
-	git clone ssh://git@localhost:3035/github/public-server.git .
-
-git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/monalisa.ed25519)"
-git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
-
-git config --local --unset user.name
-git config --local --add user.name monalisa
-
-git config --local --unset user.email
-git config --local --add user.email octocat@github.com
-
-# Clone public-server as collaborator
-
-rm -rf /workspaces/collab-public-server
-mkdir /workspaces/collab-public-server
-cd /workspaces/collab-public-server
-
-GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/collaborator.ed25519)" \
-	git clone ssh://git@localhost:3035/github/public-server.git .
-
-git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/collaborator.ed25519)"
-git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
-
-git config --local --unset user.name
-git config --local --add user.name collaborator
-
-git config --local --unset user.email
-git config --local --add user.email collaborator@github.com
-
-# Clone public-server as outsider
-
-rm -rf /workspaces/outsider-public-server
-mkdir /workspaces/outsider-public-server
-cd /workspaces/outsider-public-server
-
-GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/outsider.ed25519)" \
-	git clone ssh://git@localhost:3035/github/public-server.git .
-
-git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/outsider.ed25519)"
-git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
-
-git config --local --unset user.name
-git config --local --add user.name outsider
-
-git config --local --unset user.email
-git config --local --add user.email outsider@github.com
+clone_repo monalisa     mona     octocat
+clone_repo collaborator collab   collaborator
+clone_repo outsider     outsider outsider
 
 popd
