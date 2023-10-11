e2c815f689b3a32e4f839dca7fca5573360f56c2
Daryl <DarylCantrell@users.noreply.github.com>
2023-10-11 09:35:37 -0700

    tab->space

--- reset-public-repos.sh
+++ reset-public-repos.sh
@@ -5,28 +5,28 @@
 
 clone_repo() {
   local login=$1; shift
-	local name=$1; shift
-	local email=$1; shift
+  local name=$1; shift
+  local email=$1; shift
 
-	rm -rf /workspaces/${name}-public-server
-	mkdir /workspaces/${name}-public-server
-	cd /workspaces/${name}-public-server
+  rm -rf /workspaces/${name}-public-server
+  mkdir /workspaces/${name}-public-server
+  cd /workspaces/${name}-public-server
 
-	GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)" \
-		git clone ssh://git@localhost:3035/github/public-server.git .
+  GIT_SSH_COMMAND="ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)" \
+    git clone ssh://git@localhost:3035/github/public-server.git .
 
-	git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)"
+  git config --local --add core.sshcommand "ssh -i $(realpath ~/dotfiles/devKeys/${login}.ed25519)"
 
-	git config --local --unset user.name
-	git config --local --add user.name ${login}
+  git config --local --unset user.name
+  git config --local --add user.name ${login}
 
-	git config --local --unset user.email
-	git config --local --add user.email ${email}@github.com
+  git config --local --unset user.email
+  git config --local --add user.email ${email}@github.com
 
-	git config --local gpg.format ssh
-	git config --local user.signingkey "$(realpath ~/dotfiles/devKeys/${login}.ed25519)"
-	git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
-	git config --local commit.gpgsign true
+  git config --local gpg.format ssh
+  git config --local user.signingkey "$(realpath ~/dotfiles/devKeys/${login}.ed25519)"
+  git config --local --add gpg.ssh.allowedSignersFile "$(realpath ~/dotfiles/devKeys/allowed_signers)"
+  git config --local commit.gpgsign true
 }
 
 pushd /workspaces
