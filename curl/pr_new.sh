126c59ddd97bd5d7b4a07ebd5767fec90cae35d4
Daryl <DarylCantrell@users.noreply.github.com>
2023-10-20 13:10:03 -0700

    More changes to PR curl scripts

--- curl/pr_new.sh
+++ curl/pr_new.sh
@@ -1,13 +1,11 @@
 pr_create() {
   local base_url=`git config --local --get ghapi.url`
   [ -z "$base_url" ] && echo "Error: ghapi.url not set" && return 1
-
   local auth_token=`git config --local --get ghapi.token`
   [ -z "$auth_token" ] && echo "Error: ghapi.token not set" && return 1
 
   local source_branch=${1#"refs/heads/"}; shift
   [ -z "$source_branch" ] && echo "Usage: pr_create <source_branch> [<target_branch>]" && return 1
-
   local target_branch=${1#"refs/heads/"}; shift
   [ -z "$target_branch" ] && target_branch=main
 
@@ -19,8 +17,8 @@ pr_create() {
 
   local curl_output=
   curl_output=`curl \
-    --request POST -s \
-    --fail-with-body \
+    --request POST \
+    -s --fail-with-body \
     --header "Content-Type: application/json" \
     --header "Accept: application/vnd.github+json" \
     --header "Authorization: token $auth_token" \
@@ -44,6 +42,95 @@ pr_create() {
   fi
 }
 
+pr_create_multi() {
+  local usage = "Usage: pr_create_multi <source_branch> <count> [<target_branch>]"
+  local source_branch=$1; shift
+  [ -z "$source_branch" ] && echo $usage && return 1
+  local count=$1; shift
+  [ -z "$count" ] && echo $usage && return 1
+
+  local target_branch=$1; shift
+
+  for ((i=$count; i>=1; i--)); do
+    pr_create $source_branch"-"$i $target_branch
+  done
+}
+
+pr_approve() {
+  local base_url=`git config --local --get ghapi.url`
+  [ -z "$base_url" ] && echo "Error: ghapi.url not set" && return 1
+  local auth_token=`git config --local --get ghapi.token`
+  [ -z "$auth_token" ] && echo "Error: ghapi.token not set" && return 1
+
+  local pr_number=$1; shift
+  [ -z "$pr_number" ] && echo "Usage: pr_approve <pr_number>" && return 1
+
+  curl \
+    --request POST \
+    -s --fail-with-body \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    --data '{ "event":"APPROVE" }' \
+    $base_url"/pulls/"$pr_number"/reviews"
+}
+
+pr_approve_multi() {
+  local usage = "Usage: pr_approve_multi <count> <pr_number>"
+  local count=$1; shift
+  [ -z "$count" ] && echo $usage && return 1
+  local pr_number=$1; shift
+  [ -z "$pr_number" ] && echo $usage && return 1
+
+  for ((i=$pr_number; i<$pr_number+$count; i++)); do
+    pr_approve $i
+  done
+}
+
+pr_merge() {
+  local base_url=`git config --local --get ghapi.url`
+  [ -z "$base_url" ] && echo "Error: ghapi.url not set" && return 1
+  local auth_token=`git config --local --get ghapi.token`
+  [ -z "$auth_token" ] && echo "Error: ghapi.token not set" && return 1
+
+  pr_number=$1; shift
+  [ -z "$pr_number" ] && echo "Usage: pr_approve <pr_number> [merge|rebase|squash]" && return 1
+  merge_method=$1; shift
+
+  local curl_output=
+  curl_output=`curl \
+    -s --fail-with-body \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    $base_url"/pulls/"$pr_number`
+  local res=$?
+
+  local head_sha=
+  if [ $res -ne 0 ]; then
+    echo
+    jq -r .message <<< "$curl_output"
+    jq -r '.errors[] | .message' <<< "$curl_output"
+    return $res
+  else
+    head_sha=`jq -r .head.sha <<< "$curl_output"`
+  fi
+
+  curl \
+    --request PUT \
+    -s --fail-with-body \
+    --header "Content-Type: application/json" \
+    --header "Accept: application/vnd.github+json" \
+    --header "Authorization: token $auth_token" \
+    --data '{
+      "sha": "'$head_sha'",
+      "merge_method": "'${merge_method:-"merge"}'"
+    }' \
+    $base_url"/pulls/"$pr_number"/merge"
+}
+
+
+
 # pr_close() {
 #   base_url=$1; shift
 #   auth_token=$1; shift
@@ -61,47 +148,4 @@ pr_create() {
 #     --header "Authorization: token $auth_token" \
 #     --data '{ "state":"closed" }' \
 #     $base_url"/pulls/"$pr_number
-# }
-
-# pr_approve() {
-#   base_url=$1; shift
-#   auth_token=$1; shift
-#   pr_number=$1; shift
-
-#   if [ -z "$pr_number" ]; then
-#     usage_error "Usage: pr_approve <base_url> <auth_token> <pr_number>"
-#     return 1
-#   fi
-
-#   curl \
-#     --request POST -i \
-#     --header "Content-Type: application/json" \
-#     --header "Accept: application/vnd.github+json" \
-#     --header "Authorization: token $auth_token" \
-#     --data '{ "event":"APPROVE" }' \
-#     $base_url"/pulls/"$pr_number"/reviews"
-# }
-
-# pr_merge() {
-#   base_url=$1; shift
-#   auth_token=$1; shift
-#   pr_number=$1; shift
-#   sha=$1; shift
-#   merge_method=$1; shift
-
-#   if [ -z "$sha" ]; then
-#     usage_error "Usage: pr_merge <base_url> <auth_token> <pr_number> <sha> [<merge_method>]"
-#     return 1
-#   fi
-
-#   curl \
-#     --request PUT -i \
-#     --header "Content-Type: application/json" \
-#     --header "Accept: application/vnd.github+json" \
-#     --header "Authorization: token $auth_token" \
-#     --data '{
-#       "sha": "'$sha'",
-#       "merge_method": "'${merge_method:-"merge"}'"
-#     }' \
-#     $base_url"/pulls/"$pr_number"/merge"
 # }
