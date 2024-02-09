71c118220e8b674f3e3c5b3e3739a737068ca571
Daryl <DarylCantrell@users.noreply.github.com>
2024-02-09 07:11:38 -0800

    Update features

--- repos-features.sh
+++ repos-features.sh
@@ -1,11 +1,8 @@
 bin/toggle-feature-flag enable merge_queue_insights
 bin/toggle-feature-flag enable push_rule_insight_details
 bin/toggle-feature-flag enable merge_queue_use_parallel_finalize
+bin/toggle-feature-flag enable merge_queue_rule_engine
 
 bin/toggle-feature-flag enable push_rulesets
 bin/toggle-feature-flag enable repo_org_rules
-bin/toggle-feature-flag enable file_extension_restriction_rule
-bin/toggle-feature-flag enable file_path_restriction_rule
 bin/toggle-feature-flag enable new_restricted_commits_rule
-bin/toggle-feature-flag enable max_file_path_length_rule
-bin/toggle-feature-flag enable max_file_size_rule
