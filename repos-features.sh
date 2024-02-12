fea2decede3dec6b3c6a8b60fce8e6fc2564f0da
Daryl <DarylCantrell@users.noreply.github.com>
2024-02-12 09:40:34 -0800

    add FF

--- repos-features.sh
+++ repos-features.sh
@@ -2,6 +2,7 @@ bin/toggle-feature-flag enable merge_queue_insights
 bin/toggle-feature-flag enable push_rule_insight_details
 bin/toggle-feature-flag enable merge_queue_use_parallel_finalize
 bin/toggle-feature-flag enable merge_queue_rule_engine
+# bin/toggle-feature-flag enable merge_queue_include_check_results
 
 bin/toggle-feature-flag enable push_rulesets
 bin/toggle-feature-flag enable repo_org_rules
