cc97d77fa90b689082480e1486c37f2dd5ce0ca8
Daryl <DarylCantrell@users.noreply.github.com>
2024-10-15 12:17:59 -0700

    Add some repos FFs

--- repos-features.sh
+++ repos-features.sh
@@ -2,3 +2,6 @@ bin/toggle-feature-flag enable merge_queue_rule_engine
 bin/toggle-feature-flag enable push_rulesets
 bin/toggle-feature-flag enable repo_org_rules
 bin/toggle-feature-flag enable new_restricted_commits_rule
+bin/toggle-feature-flag enable mergebox_react_partial
+bin/toggle-feature-flag enable read_from_mergebox_json_api
+bin/toggle-feature-flag enable status_checks_json
