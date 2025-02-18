d3cdbbab17285def7b0922fd835c814ee141f8e3
Daryl <DarylCantrell@users.noreply.github.com>
2025-02-18 12:49:39 -0800

    Add some FF's

--- repos-features.sh
+++ repos-features.sh
@@ -1,6 +1,4 @@
 bin/toggle-feature-flag enable merge_queue_rule_engine
-bin/toggle-feature-flag enable push_rulesets
-bin/toggle-feature-flag enable repo_org_rules
 bin/toggle-feature-flag enable new_restricted_commits_rule
 bin/toggle-feature-flag enable mergebox_react_partial
 bin/toggle-feature-flag enable read_from_mergebox_json_api
@@ -8,3 +6,11 @@ bin/toggle-feature-flag enable status_checks_json
 bin/toggle-feature-flag enable rule_pr_required_reviewers_filter_suggestions
 bin/toggle-feature-flag enable rule_pr_required_reviewers_enforce
 bin/toggle-feature-flag enable last_reviewable_push_use_new_algo
+
+bin/toggle-feature-flag enable repo_policy_bypass
+bin/toggle-feature-flag enable member_privilege_rulesets
+bin/toggle-feature-flag enable push_rulesets
+bin/toggle-feature-flag enable repo_org_rules
+bin/toggle-feature-flag enable organization_policies
+bin/toggle-feature-flag enable push_ruleset_delegated_bypass
+bin/toggle-feature-flag enable updated_bypass_requests_ui
