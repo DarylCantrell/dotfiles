1d5b77e2b846ff7a689eaf9f7a25bab820224786
Daryl <DarylCantrell@users.noreply.github.com>
2025-11-06 12:39:49 -0800

    Fix accessibilityScanDisable.js

--- accessibilityScanDisable.js
+++ accessibilityScanDisable.js
@@ -1,4 +1,2 @@
-document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax"
-document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax; secure"
-document.cookie="accessibilityScan=false; path=/; domain=github.localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax"
-document.cookie="accessibilityScan=false; path=/; domain=github.localhost; expires=Wed, 01 Jan 2031 00:00:00 UTC; samesite=lax; secure"
+document.cookie="accessibilityScan=false; path=/; domain=localhost; expires=Wed, 01 Jan 2030 00:00:00 UTC; samesite=lax"
+document.cookie="accessibilityScan=false; path=/; domain=github.localhost; expires=Wed, 01 Jan 2030 00:00:00 UTC; samesite=lax"
